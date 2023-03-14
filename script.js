d34aa0b6650605486b85459340577187323112e1
Batas <nischal.batas88@gmail.com>
2023-03-14 23:12:42 +0545

    adds

---
+++ script.js
@@ -0,0 +1,33 @@
+const navbar = document.querySelector(".navbar-content");
+const navbarHeight = navbar.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > navbarHeight) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
+
+
+
+
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 100;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+
+
